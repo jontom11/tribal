aff116392a0906b6c2dd16e43731ff3514f721a7
James Hogan <jhogan@jhogan.net>
2017-05-13 12:32:01 -0700

    Delint colorWave.js

--- client/styles/colorWave.js
+++ client/styles/colorWave.js
@@ -1,4 +1,3 @@
-var intervalID = window.setInterval(colorWave, 20); // COMMENT/UN-COMMENT THIS LINE TO DISABLE/ENABLE COLORWAVE
 var i = 1;
 var j = 0;
 var a = 1;
@@ -6,11 +5,11 @@ var b = 1;
 var c = 1;
 var switchy = 'positive';
 
-function colorWave() {
+const colorWave = function() {
 
-  var newBackgroundColor = 'rgb(' + a + ',' + Math.floor(Math.abs(b-255)) + ',' + Math.floor(Math.abs(c)) + ')';
-  var newTextColor = 'rgb(' + (a+127) + ',' + Math.floor(Math.abs(b-127)) + ',' + Math.floor(Math.abs(c+64)) + ')';
-//  document.body.style.backgroundColor = newColor;  
+  var newBackgroundColor = 'rgb(' + a + ',' + Math.floor(Math.abs(b - 255)) + ',' + Math.floor(Math.abs(c)) + ')';
+  var newTextColor = 'rgb(' + (a + 127) + ',' + Math.floor(Math.abs(b - 127)) + ',' + Math.floor(Math.abs(c + 64)) + ')';
+  // document.body.style.backgroundColor = newColor;
   var elem = document.getElementById('tribelist');
   elem.style.backgroundColor = newBackgroundColor;
   elem.style.color = newTextColor;
@@ -23,7 +22,7 @@ function colorWave() {
   }
 
   a = i;
-  b = i*1.473;
+  b = i * 1.473;
   c = Math.cos(i * 0.05) * 255;
 
   if (switchy === 'negative') {
@@ -32,4 +31,6 @@ function colorWave() {
   if (switchy === 'positive') {
     i++;
   }
-}
+};
+
+var intervalID = window.setInterval(colorWave, 20); // COMMENT/UN-COMMENT THIS LINE TO DISABLE/ENABLE COLORWAVE
