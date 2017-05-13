c484123613c76a8f97d6c942e76e51dd7c3c883c
James Hogan <jhogan@jhogan.net>
2017-05-13 10:47:34 -0700

    Move client `playlist` messaging into tribelist component

--- client/components/main.js
+++ client/components/main.js
@@ -1,21 +1,16 @@
-const MainController = function( tribalServer, $location ) {
+const MainController = function( tribalServer ) {
 
   tribalServer.test()
     .then( (res) => {
       this.messageFromServer = res.data;
     });
-
-  tribalServer.getPlaylist( $location.search().playlist, (res) => {
-    $location.search( 'playlist', res._id );
-    this.playlist = res.songs;
-  });
 };
 
 const Main = function() {
   return {
     scope: {},
     restrict: 'E',
-    controller: [ 'tribalServer', '$location', MainController ],
+    controller: [ 'tribalServer', MainController ],
     controllerAs: 'ctrl',
     bindToController: true,
     templateUrl: '/templates/main.html',
--- client/components/tribelist.js
+++ client/components/tribelist.js
@@ -1,12 +1,20 @@
+const TribelistController = function( tribalServer, $location ) {
+
+  tribalServer.getPlaylist( $location.search().playlist, (res) => {
+    $location.search( 'playlist', res._id );
+    this.playlist = res.songs;
+  });
+};
+
 angular.module('tribal')
 
 .directive('tribelist', function() {
   return {
     scope: {
-      
+
     },
     restrict: 'E',
-    controller: function() {},
+    controller: [ 'tribalServer', '$location', TribelistController ],
     controllerAs: 'ctrl',
     bindToController: true,
     templateUrl: '/templates/tribelist.html'
