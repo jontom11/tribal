a6de4ce74cf6dcfbb07cf684ef1d77109ba451d6
aybkao <aybkao@gmail.com>
2017-05-16 23:40:41 -0700

    all passport methods in core.js

--- server/core.js
+++ server/core.js
@@ -3,6 +3,8 @@ const db = require('./database');
 const Promise = require('bluebird');
 const request = require('request');
 const mongoose = require('mongoose');
+const passport = require('passport');
+const FacebookStrategy = require('passport-facebook').Strategy;
 
 const app = express();
 const http = require('http').Server(app);
@@ -13,6 +15,66 @@ const SERVER_PORT = process.env.PORT || 4242;
 const DATABASE_CONNECTED_MESSAGE_PREFIX = 'Database connection status: ';
 const DATABASE_CONNECTED_MESSAGE = 'Connected';
 const DATABASE_NOT_CONNECTED_MESSAGE = 'NOT connected';
+
+/////////////////////// FB //////////////////////
+passport.use(new FacebookStrategy({
+  clientID: '1199458670180745',
+  clientSecret: 'b88aebdf7f7a9bbc58046048d66f4944',
+  callbackURL: '/auth/facebook/callback'
+},
+  function(accessToken, refreshToken, profile, done) {
+    console.log('accessToken', accessToken);
+    console.log('refreshToken', refreshToken);
+    console.log('profile', profile);
+
+    User.find({'facebookID': profile.id}, function(err, data) {
+      if (err) {
+        return done(err);
+      }
+      //if no data create new user with values from Google
+      if (data.length === 0) {
+        user = new User({
+          facebookID: profile.id, 
+          name: profile.displayName, 
+          email: profile.emails[0].value
+        });
+        user.save(function(err, user) {
+          if (err) console.log(err);
+          return done(err, user);
+        });
+      } else {
+        //found user. Return
+        return done(err, data);
+      }
+    });
+  }
+));
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/auth/facebook', passport.authenticate('facebook', 
+  { session: false, scope: ['profile', 'email'] }));
+
+app.get('/auth/facebook/callback', 
+  passport.authenticate('facebook', {failureRedirect: '/'}), function(req, res) {
+    res.redirect('/profile');
+  });
+
+app.get('/profile', function (req, res) {
+  console.log('we got to the profile page');
+  res.send('AUTHENTICATION OK!');
+});
+/////////////////////// FB //////////////////////
+
 
 // test endpoint for reporting status of database connection
 app.get('/test', (req, res) => {
@@ -57,10 +119,12 @@ app.get('/tracks', (req, res) => {
   });
 });
 
-app.get('/auth/facebook', (req, res) => {
-  console.log("this url points to facebook auth");
-  res.send("THIS IS AUTH URL");
-})
+// app.get('/auth/facebook', (req, res) => {
+//   console.log("this url points to facebook auth");
+//   res.send("THIS IS AUTH URL");
+// })
+
+
 
 // socket.io framework
 io.on( 'connection', function(client) {
